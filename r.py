4942caee8e1db77bd33b0587fe2f49d048d0fb8e
Albert.Wang <Albert.Wang@ADVANTECH.CORP>
2020-11-10 14:43:52 +0800

    Second Version

--- r.py
+++ r.py
@@ -1,7 +1,9 @@
 import random
 num = random.randint(1, 100)
+count = 0
 
 while True:
+	count = count + 1
 	ans = input('Please guess a number: ')
 	ans = int(ans)
 	if ans == num:
@@ -11,5 +13,5 @@ while True:
 		print('Too big, lower your answer...')
 	elif ans < num:
 		print('Too small, increase the number..')
-
+	print('This is the', count, 'time you try to find the answer!!')
 
